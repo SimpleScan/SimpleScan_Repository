e26672e7e81c16b662e0f82b8bd9252108a5d406
pearse1 <tjp1588@gmail.com>
2015-03-04 11:10:02 -0600

    pearse1 - more table additions to our contract class.

--- SimpleScan/src/com/SimpleScan/simplescan/sqlite/SimpleScanContract.java
+++ SimpleScan/src/com/SimpleScan/simplescan/sqlite/SimpleScanContract.java
@@ -12,7 +12,7 @@ public class SimpleScanContract {
 	public static final  int    DATABASE_VERSION = 1;
 	public static final  String DATABASE_NAME = "simplescan.db";
 	private static final String TEXT_TYPE = " TEXT";
-	private static final String DATE_TYPE = " TEXT";  // SQLite doesn't have a Date type (Only TEXT, REAL, INTEGER)
+	private static final String INTEGER_TYPE = " INTEGER";	
 	private static final String COMMA_SEP = ",";
 	
 	/**
@@ -40,17 +40,49 @@ public class SimpleScanContract {
 		public static final String COLUMN_NAME_AMOUNT = "amount";
 		public static final String COLUMN_NAME_DATE = "date";
 		public static final String COLUMN_NAME_TITLE = "title";
-		public static final String COLUMN_NAME_CATEGORY = "category";
-		public static final String COLUMN_NAME_IMAGEID = "imageid";
+		public static final String COLUMN_NAME_CATEGORY_ID = "categoryid";
+		public static final String COLUMN_NAME_SHARED_ID = "sharedid";
+		public static final String COLUMN_NAME_IMAGE_ID = "imageid";
 		
 		public static final String CREATE_TABLE = "CREATE TABLE " +
 	            TABLE_NAME + " (" +
 	            _ID + " INTEGER PRIMARY KEY," +
-	            COLUMN_NAME_AMOUNT + TEXT_TYPE + ", " +	            
-	            COLUMN_NAME_DATE + TEXT_TYPE + ", " +
-	            COLUMN_NAME_TITLE + TEXT_TYPE + ", " +
-	            COLUMN_NAME_CATEGORY + TEXT_TYPE + ", " +	            
-	            COLUMN_NAME_IMAGEID + TEXT_TYPE + ");";
+	            COLUMN_NAME_AMOUNT + TEXT_TYPE + COMMA_SEP +	            
+	            COLUMN_NAME_DATE + TEXT_TYPE + COMMA_SEP +
+	            COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+	            COLUMN_NAME_CATEGORY_ID + TEXT_TYPE + COMMA_SEP +	
+	            COLUMN_NAME_SHARED_ID + TEXT_TYPE + COMMA_SEP +	
+	            COLUMN_NAME_IMAGE_ID + TEXT_TYPE + ");";
+		public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+	}
+	
+	public static abstract class SharedExpenseTable implements BaseColumns {
+		public static final String TABLE_NAME = "sharedexpense";
+		public static final String COLUMN_NAME_EXPENSE_ID = "expenseid";
+		public static final String COLUMN_NAME_CONTACT_ID = "contactid";
+		public static final String COLUMN_NAME_HAS_PAID = "haspaid";
+		
+		public static final String CREATE_TABLE = "CREATE TABLE " +
+	            TABLE_NAME + " (" +
+	            _ID + " INTEGER PRIMARY KEY," +
+	            COLUMN_NAME_EXPENSE_ID + TEXT_TYPE + COMMA_SEP +	            
+	            COLUMN_NAME_CONTACT_ID + TEXT_TYPE + COMMA_SEP +
+	            COLUMN_NAME_HAS_PAID + TEXT_TYPE + ");";
+		public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+	}
+	
+	/** Represents a category table */
+	public static abstract class CategoryTable implements BaseColumns {
+		public static final String TABLE_NAME = "category";
+		public static final String COLUMN_NAME_TITLE = "title";
+		public static final String COLUMN_NAME_COLOR = "color";
+		
+		public static final String CREATE_TABLE = "CREATE TABLE " +
+	            TABLE_NAME + " (" +
+	            _ID + INTEGER_TYPE + COMMA_SEP +	            
+	            COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +	            
+	            COLUMN_NAME_COLOR + TEXT_TYPE + COMMA_SEP + 
+	            "PRIMARY KEY(_ID, " + COLUMN_NAME_TITLE + "));"; // We have two primary keys for this table
 		public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 	}
 }
