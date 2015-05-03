f67c517db40bb8eb2e63628beb51a42685b9c9c8
Tai Wu <twu15@illinois.edu>
2015-05-03 14:43:02 -0500

    Added javadoc comments to OCR, DragRectView, ZoomableImageView

--- SimpleScan/src/com/SimpleScan/simplescan/Camera/Views/DragRectView.java
+++ SimpleScan/src/com/SimpleScan/simplescan/Camera/Views/DragRectView.java
@@ -23,35 +23,66 @@ public class DragRectView extends View {
 	private OnUpCallback mCallback = null;
 	private boolean isCropping;
 	
+	/**
+	 * Check whether the user requested cropping
+	 * @return boolean that represents whether it is in the cropping mode
+	 */
 	public boolean isCroppingMode() {
     	return isCropping;
     }
     
+    /**
+     * Set the cropping mode upon user's request
+     * @param newCroppingMode
+     */
     public void setCroppingMode (boolean newCroppingMode) {
     	isCropping = newCroppingMode;
     }
 	
+	/**
+	 * @author Tai Wu
+	 *	Callback function for finishing dragging
+	 */
 	public interface OnUpCallback {
 	    void onRectFinished(Rect rect);
 	}
 
+	/**
+	 * DragRectView constructor that initialize related variables
+	 * @param context
+	 */
 	public DragRectView(final Context context) {
 	    super(context);
 	    init();
 	}
 
+	/**
+	 * DragRectView constructor that initialize related variables with specified attributes
+	 * @param context
+	 * @param attrs: attributes to be applied
+	 */
 	public DragRectView(final Context context, final AttributeSet attrs) {
 	    super(context, attrs);
 	    init(); 
 	    isCropping = false;
 	}
-
+	
+	/**
+	 * DragRectView constructor that initialize related variables with specified attributes and style
+	 * @param context
+	 * @param attrs: attributes to be applied
+	 * @param defStyle: style to be applied
+	 */
 	public DragRectView(final Context context, final AttributeSet attrs, final int defStyle) {
 	    super(context, attrs, defStyle);
 	    init(); 
 	    isCropping = false;
 	}
 	
+	/**
+	 * Set the color of the border of the rectangle
+	 * @param borderColor
+	 */
 	public void setBorderColor(int borderColor) {
 		mRectPaint.setColor(borderColor);
 	}
